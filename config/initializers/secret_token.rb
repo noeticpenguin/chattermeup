22b93af94ce2df3620ab7b5e806f2abaecfee64f
Kevin Poorman <kjp@brightleafsoftware.com>
2013-06-30 15:29:04 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chattermeup::Application.config.secret_key_base = 'ac512b84db40db408999f962cfd3d4008f52174277bab23132d20538253a449339b023378f0128fd7e6c554c7267ee1e2e204dee6db528598b16dffdfd1478f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chattermeup::Application.config.session_store :cookie_store, key: '_chattermeup_session'
